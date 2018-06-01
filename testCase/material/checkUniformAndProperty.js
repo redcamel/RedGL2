450456115815cdda31e10d8d6741f234569524a3
백선기 <webseon@gmail.com>
2018-06-01 18:01:07 +0900

    #123 - checkUniformAndProperty 테스트케이스작성 - RedProgram : 프로그램 생성시 버텍스쉐이더와 프레그먼트쉐이더에 중복된 선언이 존재할경우 에러를 밷도록 설정

---
+++ testCase/material/checkUniformAndProperty.js
@@ -0,0 +1,175 @@
+"use strict";
+RedGL( document.createElement( 'canvas' ), function ( v ) {
+	var tRedGL = this;
+	redSuite(
+		"Material 정의시 주요부분 동작테스트",
+		redGroup(
+			"생성 확인",
+			redTest( "기본 생성 테스트", function ( unit, title ) {
+				var TestMaterial;
+				(function () {
+					var makeProgram;
+					TestMaterial = function ( redGL ) {
+						if ( !(this instanceof TestMaterial) ) return new TestMaterial( redGL );
+						/////////////////////////////////////////
+						// 유니폼 프로퍼티
+						/////////////////////////////////////////
+						// 일반 프로퍼티
+						this['program'] = makeProgram( redGL );
+						this['_UUID'] = RedGL['makeUUID']();
+						this.checkUniformAndProperty()
+					}
+					makeProgram = (function () {
+						var vSource, fSource;
+						var PROGRAM_NAME;
+						vSource = function () {
+							/* @preserve
+							void main(void) {
+								gl_Position = vec4(1.0);
+							}
+							 */
+						}
+						fSource = function () {
+							/* @preserve
+							 precision mediump float;
+							 void main(void) {
+								 gl_FragColor = vec4(1.0);
+							 }
+							 */
+						}
+						vSource = RedGLUtil.getStrFromComment( vSource.toString() );
+						fSource = RedGLUtil.getStrFromComment( fSource.toString() );
+						PROGRAM_NAME = 'testProgram' + RedGL.makeUUID();
+						return function ( redGL ) {
+							return RedProgram( redGL, PROGRAM_NAME, vSource, fSource )
+						}
+					})()
+					TestMaterial.prototype = RedBaseMaterial.prototype
+					Object.freeze( TestMaterial )
+				})();
+				try {
+					var t0 = TestMaterial( tRedGL )
+					console.log( '///////////////////////////////////////////////////////////' )
+					console.log( title, '\n', t0 )
+					unit.run( true )
+				} catch ( error ) {
+					console.log( '///////////////////////////////////////////////////////////' )
+					console.log( title, '\n', error )
+					unit.run( false )
+				}
+			}, true ),
+			redTest( "유니폼 매칭 성공 테스트 : uXxxx가 쉐이더에 선언/사용되고 있으면 재질내의 xxxx 속성키가 있는지 / ex) uColor는 재질의 color속성이 있는지 매칭함", function ( unit, title ) {
+				var TestMaterial;
+				(function () {
+					var makeProgram;
+					TestMaterial = function ( redGL ) {
+						if ( !(this instanceof TestMaterial) ) return new TestMaterial( redGL );
+						/////////////////////////////////////////
+						// 유니폼 프로퍼티
+						this['testFloat'] = 1;
+						/////////////////////////////////////////
+						// 일반 프로퍼티
+						this['program'] = makeProgram( redGL );
+						this['_UUID'] = RedGL['makeUUID']();
+						this.checkUniformAndProperty()
+					}
+					makeProgram = (function () {
+						var vSource, fSource;
+						var PROGRAM_NAME;
+						vSource = function () {
+							/* @preserve
+							uniform float uTestFloat;
+							void main(void) {
+								uTestFloat;
+								gl_Position = vec4(1.0);
+							}
+							 */
+						}
+						fSource = function () {
+							/* @preserve
+							 precision mediump float;
+							 void main(void) {
+								 gl_FragColor = vec4(1.0);
+							 }
+							 */
+						}
+						vSource = RedGLUtil.getStrFromComment( vSource.toString() );
+						fSource = RedGLUtil.getStrFromComment( fSource.toString() );
+						PROGRAM_NAME = 'testProgram' + RedGL.makeUUID();
+						return function ( redGL ) {
+							return RedProgram( redGL, PROGRAM_NAME, vSource, fSource )
+						}
+					})()
+					TestMaterial.prototype = RedBaseMaterial.prototype
+					Object.freeze( TestMaterial )
+				})();
+				try {
+					var t0 = TestMaterial( tRedGL )
+					console.log( '///////////////////////////////////////////////////////////' )
+					console.log( title, '\n', t0 )
+					unit.run( true )
+				} catch ( error ) {
+					console.log( '///////////////////////////////////////////////////////////' )
+					console.log( title, '\n', error )
+					unit.run( false )
+				}
+			}, true ),
+			redTest( "유니폼 매칭 실패 테스트 : uXxxx가 쉐이더에 선언/사용되고 있으면 재질내의 xxxx 속성키가 있는지 / ex) uColor는 재질의 color속성이 있는지 매칭함", function ( unit, title ) {
+				var TestMaterial;
+				(function () {
+					var makeProgram;
+					TestMaterial = function ( redGL ) {
+						if ( !(this instanceof TestMaterial) ) return new TestMaterial( redGL );
+						/////////////////////////////////////////
+						// 유니폼 프로퍼티
+						/////////////////////////////////////////
+						// 일반 프로퍼티
+						this['program'] = makeProgram( redGL );
+						this['_UUID'] = RedGL['makeUUID']();
+						this.checkUniformAndProperty()
+
+					}
+					makeProgram = (function () {
+						var vSource, fSource;
+						var PROGRAM_NAME;
+						vSource = function () {
+							/* @preserve
+							uniform float uTestFloat;
+							void main(void) {
+								uTestFloat;
+								gl_Position = vec4(1.0);
+							}
+							 */
+						}
+						fSource = function () {
+							/* @preserve
+							 precision mediump float;
+							 void main(void) {
+								 gl_FragColor = vec4(1.0);
+							 }
+							 */
+						}
+						vSource = RedGLUtil.getStrFromComment( vSource.toString() );
+						fSource = RedGLUtil.getStrFromComment( fSource.toString() );
+						PROGRAM_NAME = 'testProgram' + RedGL.makeUUID();
+						return function ( redGL ) {
+							return RedProgram( redGL, PROGRAM_NAME, vSource, fSource )
+						}
+					})()
+					TestMaterial.prototype = RedBaseMaterial.prototype
+					Object.freeze( TestMaterial )
+				})();
+				try {
+					var t0 = TestMaterial( tRedGL )
+					console.log( '///////////////////////////////////////////////////////////' )
+					console.log( title, '\n', t0 )
+					unit.run( true )
+				} catch ( error ) {
+					console.log( '///////////////////////////////////////////////////////////' )
+					console.log( title, '\n', error )
+					unit.run( false )
+				}
+			}, false )
+		)
+	)
+} )
