4cde44ec0fac9f2a3d138afd069e2ce6b6a6c7f3
redcamel <webseon@gmail.com>
2018-07-28 18:13:43 +0900

    - RedBaseContainer 코드정리 & 테스트케이스 추가

--- src/base/RedBaseContainer.js
+++ src/base/RedBaseContainer.js
@@ -12,209 +12,195 @@ var RedBaseContainer;
 	 }
 	 :DOC*/
 	RedBaseContainer = function () {};
-	var prototypeData = {
-		sortGeometry: function (recursive) {
-			if ( recursive ) {
-				var i = this.children.length;
-				while ( i-- ) this.children[i].sortGeometry(recursive)
-			}
-			this.children.sort(function (a, b) {
-				a = a['_geometry']['interleaveBuffer']['_UUID'];
-				b = b['_geometry']['interleaveBuffer']['_UUID'];
-				if ( a < b ) return -1;
-				if ( a > b ) return 1;
-				return 0
-			})
-		},
-		sortMaterial: function (recursive) {
-			if ( recursive ) {
-				var i = this.children.length;
-				while ( i-- ) this.children[i].sortMaterial(recursive)
-			}
-			this.children.sort(function (a, b) {
-				a = a['_material']['program']['_UUID'];
-				b = b['_material']['program']['_UUID'];
-				if ( a < b ) return -1;
-				if ( a > b ) return 1;
-				return 0
-			})
-		},
-		sortGeometryAndMaterial: function (recursive) {
-			//TODO: 정의,검증 해야함
-			if ( recursive ) {
-				var i = this.children.length;
-				while ( i-- ) this.children[i].sortGeometryAndMaterial(recursive)
-			}
-			this.children.sort(function (a, b) {
-				a = a['_geometry']['interleaveBuffer']['_UUID'];
-				b = b['_geometry']['interleaveBuffer']['_UUID'];
-				if ( a == b ) {
-					var a2 = a['_material']['program']['_UUID'];
-					var b2 = b['_material']['program']['_UUID'];
-					if ( a2 < b2 ) return -1;
-					if ( a2 > b2 ) return 1;
-					return 0
-				}
-				if ( a < b ) return -1;
-				if ( a > b ) return 1;
-				return 0
-			})
-		},
-		/**DOC:
-		 {
-			 code : 'METHOD',
-			 title :`addChild`,
-			 description : `자식추가`,
-			 params:{
-				 child : [
-					 {type:'RedBaseObject3D Instance'}
-				 ]
-			 },
-			 return : 'void'
-		 }
-		 :DOC*/
-		addChild: (function () {
-			var t0;
-			return function (child) {
-				child instanceof RedBaseObject3D || RedGLUtil.throwFunc('addChild', 'RedBaseObject3D Instance만 가능', '입력값 : ' + child);
-				t0 = this['children'].indexOf(child);
-				if ( t0 != -1 ) child = this['children'].splice(t0, 1)[0];
-				this['children'].push(child);
-			}
-		})(),
-		/**DOC:
-		 {
-			 code : 'METHOD',
-			 title :`addChildAt`,
-			 description : `인덱스 위치에 자식을 추가`,
-			 params:{
-				 child : [
-					 {type:'RedBaseObject3D Instance'}
-				 ],
-				 index : [
-					 {type:'uint'}
-				 ]
-			 },
-			 return : 'void'
-		 }
-		 :DOC*/
-		addChildAt: function (child, index) {
-			typeof index == 'number' || RedGLUtil.throwFunc('addChildAt', 'index는 숫자만 입력가능', '입력값 : ' + index);
-			child instanceof RedBaseObject3D || RedGLUtil.throwFunc('addChildAt', 'RedBaseObject3D Instance만 가능', '입력값 : ' + child);
-			if ( this['children'].length >= index ) {
-				if ( index ) this['children'].splice(index, 0, child);
-				else this['children'].push(child);
-			}
-			else RedGLUtil.throwFunc('addChildAt', '해당인덱스가 현재 children 길이와 같거나 큽니다.', '입력값 : ' + index);
-		},
-		/**DOC:
-		 {
-			 code : 'METHOD',
-			 title :`removeChild`,
-			 description : `해당 자식을 제거`,
-			 params:{
-				 child : [
-					 {type:'RedBaseObject3D Instance'}
-				 ]
-			 },
-			 return : 'void'
-		 }
-		 :DOC*/
-		removeChild: (function () {
-			var t0;
-			return function (child) {
-				t0 = this['children'].indexOf(child);
-				if ( t0 == -1 ) RedGLUtil.throwFunc('removeChild', '존재하지 않는 RedMesh를 삭제하려고 함');
-				else this['children'].splice(t0, 1);
-			}
-		})(),
-		/**DOC:
-		 {
-			 code : 'METHOD',
-			 title :`removeChildAt`,
-			 description : `인덱스 위치에 있는 자식을 제거`,
-			 params:{
-				 index : [
-					 {type:'uint'}
-				 ]
-			 },
-			 return : 'void'
-		 }
-		 :DOC*/
-		removeChildAt: function (index) {
-			if ( typeof index != 'number' ) RedGLUtil.throwFunc('removeChildAt', 'index가 Number형이 아님 ', '입력값 : ' + index);
-			if ( this['children'][index] ) this['children'].splice(index, 1);
-			else RedGLUtil.throwFunc('removeChildAt', 'index 해당인덱스에 위치한 자식이 없음.', '입력값 : ' + index);
-		},
-		/**DOC:
-		 {
-			 code : 'METHOD',
-			 title :`removeChildAll`,
-			 description : `전체 자식을 제거`,
-			 return : 'void'
-		 }
-		 :DOC*/
-		removeChildAll: function () {
-			this['children'].length = 0
-		},
-		/**DOC:
-		 {
-			 code : 'METHOD',
-			 title :`getChildAt`,
-			 description : `해당위치의 자식을 반환`,
-			 params:{
-				 index : [
-					 {type:'uint'}
-				 ]
-			 },
-			 return : 'RedBaseObject3D Instance'
-		 }
-		 :DOC*/
-		getChildAt: function (index) {
-			if ( typeof index != 'number' ) RedGLUtil.throwFunc('getChildAt', 'index가 Number형이 아님 ');
-			return this['children'][index];
-		},
-		/**DOC:
-		 {
-			 code : 'METHOD',
-			 title :`getChildIndex`,
-			 description : `해당객체의 인덱스 번호를 반환`,
-			 params:{
-				 child : [
-					 {type:'RedBaseObject3D Instance'}
-				 ]
-			 },
-			 return : 'int'
-		 }
-		 :DOC*/
-		getChildIndex: function (child) {
-			return this['children'].indexOf(child);
-		},
-		// getChildByName: function () {
-		//     //TODO:
-		// },
-		// setChildIndex: function (v) {
-		//     //TODO:
-		// },
-		/**DOC:
-		 {
-			 code : 'METHOD',
-			 title :`numChildren`,
-			 description : `numChildren`,
-			 params:{
-				 target : [
-					 {type:'RedBaseObject3D Instance'}
-				 ]
-			 },
-
-			 return : 'uint'
-		 }
-		 :DOC*/
-		numChildren: function () {
-			return this['children'].length;
-		}
-	};
 	RedBaseContainer.prototype = new RedBaseObject3D();
-	for ( var k in prototypeData ) RedBaseContainer.prototype[k] = prototypeData[k];
+	RedBaseContainer.prototype['sortGeometry'] = function (recursive) {
+		if ( recursive ) {
+			var i = this.children.length;
+			while ( i-- ) this.children[i].sortGeometry(recursive)
+		}
+		this.children.sort(function (a, b) {
+			a = a['_geometry']['interleaveBuffer']['_UUID'];
+			b = b['_geometry']['interleaveBuffer']['_UUID'];
+			if ( a < b ) return -1;
+			if ( a > b ) return 1;
+			return 0
+		})
+	};
+	RedBaseContainer.prototype['sortMaterial'] = function (recursive) {
+		if ( recursive ) {
+			var i = this.children.length;
+			while ( i-- ) this.children[i].sortMaterial(recursive)
+		}
+		this.children.sort(function (a, b) {
+			a = a['_material']['program']['_UUID'];
+			b = b['_material']['program']['_UUID'];
+			if ( a < b ) return -1;
+			if ( a > b ) return 1;
+			return 0
+		})
+	};
+	RedBaseContainer.prototype['sortGeometryAndMaterial'] = function (recursive) {
+		//TODO: 정의,검증 해야함
+		if ( recursive ) {
+			var i = this.children.length;
+			while ( i-- ) this.children[i].sortGeometryAndMaterial(recursive)
+		}
+		this.children.sort(function (a, b) {
+			a = a['_geometry']['interleaveBuffer']['_UUID'];
+			b = b['_geometry']['interleaveBuffer']['_UUID'];
+			if ( a == b ) {
+				var a2 = a['_material']['program']['_UUID'];
+				var b2 = b['_material']['program']['_UUID'];
+				if ( a2 < b2 ) return -1;
+				if ( a2 > b2 ) return 1;
+				return 0
+			}
+			if ( a < b ) return -1;
+			if ( a > b ) return 1;
+			return 0
+		})
+	};
+	/**DOC:
+	 {
+		 code : 'METHOD',
+		 title :`addChild`,
+		 description : `자식추가`,
+		 params:{
+			 child : [
+				 {type:'RedBaseObject3D Instance'}
+			 ]
+		 },
+		 return : 'void'
+	 }
+	 :DOC*/
+	RedBaseContainer.prototype['addChild'] = function (child) {
+		child instanceof RedBaseObject3D || RedGLUtil.throwFunc('addChild', 'RedBaseObject3D Instance만 가능', '입력값 : ' + child);
+		if ( this['children'].indexOf(child) > -1 ) this['removeChild'](child);
+		this['children'].push(child);
+	};
+	/**DOC:
+	 {
+		 code : 'METHOD',
+		 title :`addChildAt`,
+		 description : `인덱스 위치에 자식을 추가`,
+		 params:{
+			 child : [
+				 {type:'RedBaseObject3D Instance'}
+			 ],
+			 index : [
+				 {type:'uint'}
+			 ]
+		 },
+		 return : 'void'
+	 }
+	 :DOC*/
+	RedBaseContainer.prototype['addChildAt'] = function (child, index) {
+		typeof index == 'number' || RedGLUtil.throwFunc('addChildAt', 'index는 숫자만 입력가능', '입력값 : ' + index);
+		child instanceof RedBaseObject3D || RedGLUtil.throwFunc('addChildAt', 'RedBaseObject3D Instance만 가능', '입력값 : ' + child);
+		if ( this['children'].indexOf(child) > -1 ) this['removeChild'](child);
+		if ( this['children'].length < index ) index = this['children'].length;
+		if ( index ) this['children'].splice(index, 0, child);
+		else this['children'].push(child);
+	};
+	/**DOC:
+	 {
+		 code : 'METHOD',
+		 title :`removeChild`,
+		 description : `해당 자식을 제거`,
+		 params:{
+			 child : [
+				 {type:'RedBaseObject3D Instance'}
+			 ]
+		 },
+		 return : 'void'
+	 }
+	 :DOC*/
+	RedBaseContainer.prototype['removeChild'] = (function () {
+		var t0;
+		return function (child) {
+			t0 = this['children'].indexOf(child);
+			if ( t0 == -1 ) RedGLUtil.throwFunc('removeChild', '존재하지 않는 RedMesh를 삭제하려고 함');
+			else this['children'].splice(t0, 1);
+		}
+	})();
+	/**DOC:
+	 {
+		 code : 'METHOD',
+		 title :`removeChildAt`,
+		 description : `인덱스 위치에 있는 자식을 제거`,
+		 params:{
+			 index : [
+				 {type:'uint'}
+			 ]
+		 },
+		 return : 'void'
+	 }
+	 :DOC*/
+	RedBaseContainer.prototype['removeChildAt'] = function (index) {
+		if ( typeof index != 'number' ) RedGLUtil.throwFunc('removeChildAt', 'index가 Number형이 아님 ', '입력값 : ' + index);
+		if ( this['children'][index] ) this['children'].splice(index, 1);
+		else RedGLUtil.throwFunc('removeChildAt', 'index 해당인덱스에 위치한 자식이 없음.', '입력값 : ' + index);
+	};
+	/**DOC:
+	 {
+		 code : 'METHOD',
+		 title :`removeChildAll`,
+		 description : `전체 자식을 제거`,
+		 return : 'void'
+	 }
+	 :DOC*/
+	RedBaseContainer.prototype['removeChildAll'] = function () {
+		this['children'].length = 0
+	};
+	/**DOC:
+	 {
+		 code : 'METHOD',
+		 title :`getChildAt`,
+		 description : `해당위치의 자식을 반환`,
+		 params:{
+			 index : [
+				 {type:'uint'}
+			 ]
+		 },
+		 return : 'RedBaseObject3D Instance'
+	 }
+	 :DOC*/
+	RedBaseContainer.prototype['getChildAt'] = function (index) {
+		typeof index == 'number' || RedGLUtil.throwFunc('getChildAt', 'index가 Number형이 아님 ');
+		return this['children'][index];
+	};
+	/**DOC:
+	 {
+		 code : 'METHOD',
+		 title :`getChildIndex`,
+		 description : `해당객체의 인덱스 번호를 반환`,
+		 params:{
+			 child : [
+				 {type:'RedBaseObject3D Instance'}
+			 ]
+		 },
+		 return : 'int'
+	 }
+	 :DOC*/
+	RedBaseContainer.prototype['getChildIndex'] = function (child) {
+		return this['children'].indexOf(child);
+	};
+	/**DOC:
+	 {
+		 code : 'METHOD',
+		 title :`numChildren`,
+		 description : `numChildren`,
+		 params:{
+			 target : [
+				 {type:'RedBaseObject3D Instance'}
+			 ]
+		 },
+
+		 return : 'uint'
+	 }
+	 :DOC*/
+	RedBaseContainer.prototype['numChildren'] = function () {
+		return this['children'].length;
+	};
 	Object.freeze(RedBaseContainer);
 })();
--- testCase/base/RedBaseContainer.js
+++ testCase/base/RedBaseContainer.js
@@ -4,25 +4,57 @@ RedGL(document.createElement('canvas'), function (v) {
 	redSuite(
 		"RedBaseContainer Method 테스트",
 		redGroup(
-			"생성 확인",
-			redTest("기본 생성 테스트", function (unit, title) {
+			"(RedBaseContainer 확장 객체 인스턴스).<b>addChild</b>( child )",
+			redTest("성공테스트 : child - RedBaseObject3D Instance만 허용하는지.", function (unit, title) {
 				try {
-					var t0 = new RedMesh(tRedGL);
-					console.log(t0)
+					var rootMesh = RedMesh(tRedGL);
+					rootMesh.addChild(RedMesh(tRedGL))
 					unit.run(true)
 				} catch ( error ) {
 					console.log('///////////////////////////////////////////////////////////')
 					console.log(title, '\n', error)
 					unit.run(false)
 				}
+			}, true),
+			redTest("실패테스트  : child - 미입력", function (unit, title) {
+				try {
+					var rootMesh = RedMesh(tRedGL);
+					rootMesh.addChild(1)
+					unit.run(true)
+				} catch ( error ) {
+					console.log('///////////////////////////////////////////////////////////')
+					console.log(title, '\n', error)
+					unit.run(false)
+				}
+			}, false),
+			redTest("실패테스트  : child - 숫자입력", function (unit, title) {
+				try {
+					var rootMesh = RedMesh(tRedGL);
+					rootMesh.addChild(1)
+					unit.run(true)
+				} catch ( error ) {
+					console.log('///////////////////////////////////////////////////////////')
+					console.log(title, '\n', error)
+					unit.run(false)
+				}
+			}, false),
+			redTest("성공테스트  : 기존에 존재하는 자식을 다시 추가할경우 : 기존을 제거하고 후방추가함", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
+				var t1 = RedMesh(tRedGL)
+				rootMesh.addChild(t1)
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(t1)
+				unit.run(rootMesh['children'][2] == t1)
 			}, true)
 		),
 		redGroup(
-			"addChild",
-			redTest("RedBaseObject3D Instance만 허용하는지.", function (unit, title) {
+			"(RedBaseContainer 확장 객체 인스턴스).<b>addChildAt</b>( child, index )",
+			redTest("성공테스트 : child - RedBaseObject3D Instance만 허용하는지.", function (unit, title) {
 				try {
-					var t0 = new RedMesh(tRedGL);
-					t0.addChild(RedMesh(tRedGL))
+					var rootMesh = RedMesh(tRedGL);
+					rootMesh.addChildAt(RedMesh(tRedGL), 0)
+					console.log(rootMesh)
 					unit.run(true)
 				} catch ( error ) {
 					console.log('///////////////////////////////////////////////////////////')
@@ -30,37 +62,11 @@ RedGL(document.createElement('canvas'), function (v) {
 					unit.run(false)
 				}
 			}, true),
-			redTest("RedBaseObject3D Instance만 허용하는지 : 실패테스트(숫자입력)", function (unit, title) {
+			redTest("실패테스트 : child - 숫자입력", function (unit, title) {
 				try {
-					var t0 = new RedMesh(tRedGL);
-					t0.addChild(1)
-					unit.run(true)
-				} catch ( error ) {
-					console.log('///////////////////////////////////////////////////////////')
-					console.log(title, '\n', error)
-					unit.run(false)
-				}
-			}, false)
-		),
-		redGroup(
-			"addChildAt",
-			redTest("RedBaseObject3D Instance만 허용하는지.", function (unit, title) {
-				try {
-					var t0 = new RedMesh(tRedGL);
-					t0.addChildAt(RedMesh(tRedGL), 0)
-					console.log(t0)
-					unit.run(true)
-				} catch ( error ) {
-					console.log('///////////////////////////////////////////////////////////')
-					console.log(title, '\n', error)
-					unit.run(false)
-				}
-			}, true),
-			redTest("RedBaseObject3D Instance만 허용하는지 : 실패테스트(숫자입력)", function (unit, title) {
-				try {
-					var t0 = new RedMesh(tRedGL);
-					t0.addChildAt(1, 1)
-					console.log(t0)
+					var rootMesh = RedMesh(tRedGL);
+					rootMesh.addChildAt(1, 0)
+					console.log(rootMesh)
 					unit.run(true)
 				} catch ( error ) {
 					console.log('///////////////////////////////////////////////////////////')
@@ -68,76 +74,10 @@ RedGL(document.createElement('canvas'), function (v) {
 					unit.run(false)
 				}
 			}, false),
-			redTest("index위치에 정확히 들어가는지", function (unit, title) {
-				var t0 = new RedMesh(tRedGL);
-				var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChildAt(t1, 2);
-				console.log(t0);
-				unit.run(t0['children'][2] == t1);
-			}, true),
-			redTest("index위치에 정확히 들어가는지", function (unit, title) {
-				var t0 = new RedMesh(tRedGL);
-				var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChildAt(t1, 4);
-				console.log(t0);
-				unit.run(t0['children'][4] == t1);
-			}, true),
-			redTest("추가후 기존자식들은 유지하면서 길이가 늘어나는지", function (unit, title) {
-				var t0 = new RedMesh(tRedGL);
-				var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChild(RedMesh(tRedGL));
-				t0.addChildAt(t1, 1);
-				console.log(t0);
-				unit.run(t0['children'][1] == t1 && t0['children'].length == 5);
-			}, true),
-			redTest("범위를 벗어나는 인덱스를 입력했을때 방어하는지", function (unit, title) {
+			redTest("실패테스트  : index - 미입력", function (unit, title) {
 				try {
-					var t0 = new RedMesh(tRedGL);
-					var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
-					t0.addChildAt(t1, 10);
-					console.log(t0);
-					unit.run(true)
-				} catch ( error ) {
-					console.log('///////////////////////////////////////////////////////////')
-					console.log(title, '\n', error)
-					unit.run(false)
-				}
-			}, false)
-		),
-		redGroup(
-			"removeChild",
-			redTest("기본 작동확인", function (unit, title) {
-				try {
-					var t0 = new RedMesh(tRedGL);
-					var t1 = RedMesh(tRedGL);
-					t0.addChild(t1)
-					t0.removeChild(t1)
-					console.log(t0)
-					unit.run(true)
-				} catch ( error ) {
-					console.log('///////////////////////////////////////////////////////////')
-					console.log(title, '\n', error)
-					unit.run(false)
-				}
-			}, true),
-			redTest("children에 존재하지않는 녀석을 삭제하려할때", function (unit, title) {
-				try {
-					var t0 = new RedMesh(tRedGL);
-					var t1 = RedMesh(tRedGL);
-					t0.addChild(t1)
-					t0.removeChild(0)
-					console.log(t0)
+					var rootMesh = RedMesh(tRedGL);
+					rootMesh.addChildAt(RedMesh(tRedGL))
 					unit.run(true)
 				} catch ( error ) {
 					console.log('///////////////////////////////////////////////////////////')
@@ -145,52 +85,145 @@ RedGL(document.createElement('canvas'), function (v) {
 					unit.run(false)
 				}
 			}, false),
-			redTest("삭제후 children.length가 잘줄어드는지", function (unit, title) {
-				var t0 = new RedMesh(tRedGL);
+			redTest("실패테스트  : index - 문자입력", function (unit, title) {
+				try {
+					var rootMesh = RedMesh(tRedGL);
+					rootMesh.addChildAt(RedMesh(tRedGL), 'failTest')
+					unit.run(true)
+				} catch ( error ) {
+					console.log('///////////////////////////////////////////////////////////')
+					console.log(title, '\n', error)
+					unit.run(false)
+				}
+			}, false),
+
+			redTest("성공테스트 : index - index위치에 정확히 들어가는지", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
 				var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(t1)
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.removeChild(t1)
-				console.log(t0)
-				unit.run(t0['children'].length)
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChildAt(t1, 2);
+				console.log(rootMesh);
+				unit.run(rootMesh['children'][2] == t1);
+			}, true),
+			redTest("성공테스트 : index - index위치에 정확히 들어가는지", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
+				var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChildAt(t1, 4);
+				console.log(rootMesh);
+				unit.run(rootMesh['children'][4] == t1);
+			}, true),
+			redTest("성공테스트 : index - 추가후 기존자식들은 유지하면서 길이가 늘어나는지", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
+				var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL));
+				rootMesh.addChildAt(t1, 1);
+				console.log(rootMesh);
+				unit.run(rootMesh['children'][1] == t1 && rootMesh['children'].length == 5);
+			}, true),
+			redTest("성공테스트  : 기존에 존재하는 자식을 다시 추가할경우 : 기존을 제거하고 후방추가함", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
+				var t1 = RedMesh(tRedGL)
+				rootMesh.addChild(t1)
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChildAt(t1, 3)
+				console.log(rootMesh['children'])
+				unit.run(rootMesh['children'][2] == t1)
+			}, true),
+			redTest("성공테스트 : index - 범위를 벗어나는 인덱스를 마지막순서에 입력", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
+				var t1 = RedMesh(tRedGL);
+				rootMesh.addChildAt(t1, 10);
+				unit.run(rootMesh['children'][0] == t1)
+				console.log(rootMesh['children'])
+			}, true)
+		),
+		redGroup(
+			"(RedBaseContainer 확장 객체 인스턴스).<b>removeChild</b>( child )",
+			redTest("성공테스트 : 기본 동작 확인", function (unit, title) {
+				try {
+					var rootMesh = RedMesh(tRedGL);
+					var t1 = RedMesh(tRedGL);
+					rootMesh.addChild(t1)
+					rootMesh.removeChild(t1)
+					console.log(rootMesh)
+					unit.run(true)
+				} catch ( error ) {
+					console.log('///////////////////////////////////////////////////////////')
+					console.log(title, '\n', error)
+					unit.run(false)
+				}
+			}, true),
+			redTest("실패테스트 : children에 존재하지않는 녀석을 삭제하려할때", function (unit, title) {
+				try {
+					var rootMesh = RedMesh(tRedGL);
+					var t1 = RedMesh(tRedGL);
+					rootMesh.addChild(t1)
+					rootMesh.removeChild(0)
+					console.log(rootMesh)
+					unit.run(true)
+				} catch ( error ) {
+					console.log('///////////////////////////////////////////////////////////')
+					console.log(title, '\n', error)
+					unit.run(false)
+				}
+			}, false),
+			redTest("성공테스트 : 삭제후 children.length가 잘줄어드는지", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
+				var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(t1)
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.removeChild(t1)
+				console.log(rootMesh)
+				unit.run(rootMesh['children'].length)
 			}, 4)
 		),
 		redGroup(
-			"removeChildAt",
-			redTest("기본 작동확인", function (unit, title) {
-				var t0 = new RedMesh(tRedGL);
+			"(RedBaseContainer 확장 객체 인스턴스).<b>removeChildAt</b>( index )",
+			redTest("성공테스트 : 기본 동작확인", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
 				var t1 = RedMesh(tRedGL);
-				t0.addChild(t1)
-				t0.removeChildAt(0)
-				console.log(t0)
-				unit.run(t0.children.length)
+				rootMesh.addChild(t1)
+				rootMesh.removeChildAt(0)
+				console.log(rootMesh)
+				unit.run(rootMesh.children.length)
 			}, 0),
-			redTest("삭제후 children.length가 잘줄어드는지", function (unit, title) {
-				var t0 = new RedMesh(tRedGL);
+			redTest("성공테스트 : 삭제후 children.length가 잘줄어드는지", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
 				var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(t1)
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.removeChildAt(2)
-				console.log(t0)
-				unit.run(t0['children'].length)
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(t1)
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.removeChildAt(2)
+				console.log(rootMesh)
+				unit.run(rootMesh['children'].length)
 			}, 4),
-			redTest("해당인덱스에 자식이 존재하지않을때", function (unit, title) {
+			redTest("실패테스트  : 해당인덱스에 자식이 존재하지않을때", function (unit, title) {
 				try {
-					var t0 = new RedMesh(tRedGL);
+					var rootMesh = RedMesh(tRedGL);
 					var t1 = RedLine(tRedGL, RedColorMaterial(tRedGL));
-					t0.addChild(RedMesh(tRedGL))
-					t0.addChild(RedMesh(tRedGL))
-					t0.addChild(t1)
-					t0.addChild(RedMesh(tRedGL))
-					t0.addChild(RedMesh(tRedGL))
-					t0.removeChildAt(10)
-					console.log(t0)
+					rootMesh.addChild(RedMesh(tRedGL))
+					rootMesh.addChild(RedMesh(tRedGL))
+					rootMesh.addChild(t1)
+					rootMesh.addChild(RedMesh(tRedGL))
+					rootMesh.addChild(RedMesh(tRedGL))
+					rootMesh.removeChildAt(10)
+					console.log(rootMesh)
 					unit.run(true)
 				} catch ( error ) {
 					console.log('///////////////////////////////////////////////////////////')
@@ -200,30 +233,30 @@ RedGL(document.createElement('canvas'), function (v) {
 			}, false)
 		),
 		redGroup(
-			"removeChildAll",
-			redTest("기본 작동확인", function (unit, title) {
-				var t0 = new RedMesh(tRedGL);
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.removeChildAll()
-				console.log(t0)
-				unit.run(t0.children.length)
+			"(RedBaseContainer 확장 객체 인스턴스).<b>removeChildAll</b>( index )",
+			redTest("성공테스트 : 기본 동작 확인", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.removeChildAll()
+				console.log(rootMesh)
+				unit.run(rootMesh.children.length)
 			}, 0)
 		),
 		redGroup(
-			"numChildren",
-			redTest("기본 작동확인", function (unit, title) {
-				var t0 = new RedMesh(tRedGL);
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				t0.addChild(RedMesh(tRedGL))
-				console.log(t0)
-				unit.run(t0.numChildren())
+			"(RedBaseContainer 확장 객체 인스턴스).<b>numChildren</b>( index )",
+			redTest("성공테스트 : 기본 동작 확인", function (unit, title) {
+				var rootMesh = RedMesh(tRedGL);
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				rootMesh.addChild(RedMesh(tRedGL))
+				console.log(rootMesh)
+				unit.run(rootMesh.numChildren())
 			}, 5)
 		)
 	)
