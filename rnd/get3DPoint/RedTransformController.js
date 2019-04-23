fb42761dbf5531b3f88ad708e0b4e8a07497e46a
백선기 <webseon@gmail.com>
2019-04-23 17:43:52 +0900

    #609 #602

--- rnd/get3DPoint/RedTransformController.js
+++ rnd/get3DPoint/RedTransformController.js
@@ -29,7 +29,7 @@ var RedTransformController;
     RedTransformController = function (redGL) {
         if (!(this instanceof RedTransformController)) return new RedTransformController(redGL);
         redGL instanceof RedGL || RedGLUtil.throwFunc('RedTransformController : RedGL Instance만 허용.', redGL);
-        var tArrowMesh;
+        var tArrowMesh, tPlaneMesh;
         var tAxis;
         var tBox, tArrow;
         var tMatX, tMatY, tMatZ;
@@ -43,6 +43,7 @@ var RedTransformController;
         // xAxis
         tArrowMesh = RedMesh(redGL, tArrow, tMatX);
         tAxis = RedMesh(redGL, tBox, tMatX);
+        // tArrowMesh['depthTestFunc'] = redGL.gl.ALWAYS
         tAxis.scaleX = tAxis.scaleY = tAxis.scaleZ = 0.1;
         tAxis.scaleX = 5;
         tArrowMesh.x = 5;
@@ -55,6 +56,7 @@ var RedTransformController;
         // yAxis
         tArrowMesh = RedMesh(redGL, tArrow, tMatY);
         tAxis = RedMesh(redGL, tBox, tMatY);
+        // tArrowMesh['depthTestFunc'] = redGL.gl.ALWAYS
         tAxis.scaleX = tAxis.scaleY = tAxis.scaleZ = 0.1;
         tAxis.scaleY = 5;
         tArrowMesh.y = 5;
@@ -66,6 +68,7 @@ var RedTransformController;
         // zAxis
         tArrowMesh = RedMesh(redGL, tArrow, tMatZ);
         tAxis = RedMesh(redGL, tBox, tMatZ);
+        tArrowMesh['depthTestFunc'] = redGL.gl.ALWAYS
         tAxis.scaleX = tAxis.scaleY = tAxis.scaleZ = 0.1;
         tAxis.scaleZ = 5;
         tArrowMesh.z = 5;
@@ -75,7 +78,68 @@ var RedTransformController;
         this['children'].push(tAxis);
         this['children'].push(tArrowMesh);
         ////////////////////////////////////////////
-        this['children'].push(RedMesh(redGL, RedSphere(redGL, 0.25, 16, 16, 16), RedColorMaterial(redGL, '#ff00ff')));
+
+        tPlaneMesh = RedMesh(redGL, RedPlane(redGL), RedColorMaterial(redGL, '#ef8d39', 0.5));
+        tPlaneMesh.x = 0.55;
+        tPlaneMesh.y = 0.55;
+        tPlaneMesh['useCullFace'] = false;
+        // tPlaneMesh['depthTestFunc'] = redGL.gl.ALWAYS
+        this['moveXY'] = tPlaneMesh
+        // this['children'].push(tPlaneMesh)
+        tPlaneMesh = RedMesh(redGL, RedPlane(redGL), RedColorMaterial(redGL, '#ef8d39', 0.5));
+        tPlaneMesh.rotationY = 90
+        tPlaneMesh.z = 0.55;
+        tPlaneMesh.y = 0.55;
+        tPlaneMesh['useCullFace'] = false;
+        // tPlaneMesh['depthTestFunc'] = redGL.gl.ALWAYS
+        // this['children'].push(tPlaneMesh)
+        this['moveYZ'] = tPlaneMesh
+        tPlaneMesh = RedMesh(redGL, RedPlane(redGL), RedColorMaterial(redGL, '#ef8d39', 0.5));
+        tPlaneMesh.rotationX = 90
+        tPlaneMesh.x = 0.55;
+        tPlaneMesh.z = 0.55;
+        tPlaneMesh['useCullFace'] = false;
+        // tPlaneMesh['depthTestFunc'] = redGL.gl.ALWAYS
+        // this['children'].push(tPlaneMesh)
+        this['moveXZ'] = tPlaneMesh
+        //
+        tPlaneMesh = RedMesh(redGL, RedSphere(redGL, 0.5, 32, 32, 32), RedColorMaterial(redGL, '#5b52aa', 1));
+        tPlaneMesh['depthTestFunc'] = redGL.gl.ALWAYS
+        this['move'] = tPlaneMesh
+        this['children'].push(tPlaneMesh)
+        ////////////////////////////////////////////
+
+        var rotationXLine = RedLine(redGL, RedColorMaterial(redGL));
+        var rotationYLine = RedLine(redGL, RedColorMaterial(redGL, '#00ff00'));
+        var rotationZLine = RedLine(redGL, RedColorMaterial(redGL, '#0000ff'));
+        var i = 36;
+        var PER = Math.PI * 2 / i
+        i = 36
+        while (i--) rotationXLine.addPoint(0, Math.sin(PER * i), Math.cos(PER * i));
+        rotationXLine.addPoint(0, Math.sin(PER * i), Math.cos(PER * i));
+        i = 36
+        while (i--) rotationYLine.addPoint(Math.sin(PER * i), 0, Math.cos(PER * i));
+        rotationYLine.addPoint(Math.sin(PER * i), 0, Math.cos(PER * i));
+        i = 36
+        while (i--) rotationZLine.addPoint(Math.sin(PER * i), Math.cos(PER * i), 0);
+        rotationZLine.addPoint(Math.sin(PER * i), Math.cos(PER * i), 0);
+        var tSphereMesh
+        tSphereMesh = RedMesh(redGL, RedSphere(redGL, 0.2, 32, 32, 32), RedColorMaterial(redGL, '#ff0000', 1));
+        tSphereMesh.y = 1
+        rotationXLine.addChild(tSphereMesh)
+        tSphereMesh = RedMesh(redGL, RedSphere(redGL, 0.2, 32, 32, 32), RedColorMaterial(redGL, '#00ff00', 1));
+        tSphereMesh.z = 1
+        rotationYLine.addChild(tSphereMesh)
+        tSphereMesh = RedMesh(redGL, RedSphere(redGL, 0.2, 32, 32, 32), RedColorMaterial(redGL, '#0000ff', 1));
+        tSphereMesh.x = 1
+        rotationZLine.addChild(tSphereMesh)
+        this['rotationXLine'] = rotationXLine
+        this['rotationYLine'] = rotationYLine
+        this['rotationZLine'] = rotationZLine
+        this['children'].push(rotationXLine)
+        this['children'].push(rotationYLine)
+        this['children'].push(rotationZLine)
+        ///////////////////////////////////////////
 
         this['_UUID'] = RedGL.makeUUID();
     };
@@ -86,103 +150,64 @@ var RedTransformController;
         var tScene = tView['scene'];
         var tController = tView['camera'];
         var tDirection = 0
-        var gap = []
-        var startPosition2 = []
+        var startPosition = []
+        var startControllerPosition = []
+        var startRotation = []
         var hd_move = function (e) {
-            var t0 = RedGLUtil.screenToWorld(
+            var currentPosition = RedGLUtil.screenToWorld(
                 [
                     e.layerX, e.layerY,
                     tView['_viewRect'][2], tView['_viewRect'][3]
                 ],
                 tController
-            )
-            if (tDirection === 0) tTransformController.x = tMesh.x = startPosition2[0] + (t0[0] - gap[0])
-            if (tDirection === 1) tTransformController.y = tMesh.y = startPosition2[1] + (t0[1] - gap[1])
-            if (tDirection === 2) tTransformController.z = tMesh.z = startPosition2[2] + (t0[2] - gap[2])
-        }
-        tScene.mouseManager.add(tTransformController['arrowX'], 'down', function (e) {
+            );
 
+            if (tDirection === 0) tTransformController.x = tMesh.x = startControllerPosition[0] + (currentPosition[0] - startPosition[0]);
+            if (tDirection === 1) tTransformController.y = tMesh.y = startControllerPosition[1] + (currentPosition[1] - startPosition[1]);
+            if (tDirection === 2) tTransformController.z = tMesh.z = startControllerPosition[2] + (currentPosition[2] - startPosition[2]);
+            if (tDirection === 3) {
+                tTransformController.x = tMesh.x = startControllerPosition[0] + (currentPosition[0] - startPosition[0]);
+                tTransformController.y = tMesh.y = startControllerPosition[1] + (currentPosition[1] - startPosition[1]);
+                tTransformController.z = tMesh.z = startControllerPosition[2] + (currentPosition[2] - startPosition[2]);
+            }
+            if (tDirection === 4) {
 
-            tDirection = 0
-            tTransformController.x = tMesh.x
-            tTransformController.y = tMesh.y
-            tTransformController.z = tMesh.z
-            console.log(e)
+            }
+        };
+        [
+            tTransformController['arrowX'], tTransformController['arrowY'], tTransformController['arrowZ'], tTransformController['move'],
+            tTransformController['rotationXLine'].getChildAt(0), tTransformController['rotationYLine'].getChildAt(0), tTransformController['rotationZLine'].getChildAt(0)
 
-            gap = RedGLUtil.screenToWorld(
-                [
-                    e.nativeEvent.layerX, e.nativeEvent.layerY,
-                    tView['_viewRect'][2], tView['_viewRect'][3]
-                ],
-                tController
-            )
-            startPosition2 = [tTransformController.x, tTransformController.y, tTransformController.z]
+        ].forEach(function (v, index) {
+            tScene.mouseManager.add(v, 'down', function (e) {
+                tDirection = index
+                tTransformController.x = tMesh.x
+                tTransformController.y = tMesh.y
+                tTransformController.z = tMesh.z
+                startRotation = [tMesh.rotationX, tMesh.rotationY, tMesh.rotationZ]
+                console.log(e)
+                startPosition = RedGLUtil.screenToWorld(
+                    [
+                        e.nativeEvent.layerX, e.nativeEvent.layerY,
+                        tView['_viewRect'][2], tView['_viewRect'][3]
+                    ],
+                    tController
+                );
+                startControllerPosition = [tTransformController.x, tTransformController.y, tTransformController.z]
 
-            if (tController.camera) tController.needUpdate = false
-            document.body.addEventListener(
-                'mousemove', hd_move
-            )
-            window.addEventListener('click', function () {
-                if (tController.camera) tController.needUpdate = true
-                document.body.removeEventListener(
+                if (tController.camera) tController.needUpdate = false
+                document.body.addEventListener(
                     'mousemove', hd_move
                 )
-            })
-        });
-        tScene.mouseManager.add(tTransformController['arrowY'], 'down', function (e) {
-
-            tDirection = 1
-            tTransformController.x = tMesh.x
-            tTransformController.y = tMesh.y
-            tTransformController.z = tMesh.z
-            console.log(e)
-
-            gap = RedGLUtil.screenToWorld(
-                [
-                    e.nativeEvent.layerX, e.nativeEvent.layerY,
-                    tView['_viewRect'][2], tView['_viewRect'][3]
-                ],
-                tController
-            )
-            startPosition2 = [tTransformController.x, tTransformController.y, tTransformController.z]
-            if (tController.camera) tController.needUpdate = false
-            document.body.addEventListener(
-                'mousemove', hd_move
-            )
-            window.addEventListener('click', function () {
-                if (tController.camera) tController.needUpdate = true
-                document.body.removeEventListener(
-                    'mousemove', hd_move
-                )
-            })
-        });
-        tScene.mouseManager.add(tTransformController['arrowZ'], 'down', function (e) {
-
-            tDirection = 2
-            tTransformController.x = tMesh.x
-            tTransformController.y = tMesh.y
-            tTransformController.z = tMesh.z
-            console.log(e)
-
-            gap = RedGLUtil.screenToWorld(
-                [
-                    e.nativeEvent.layerX, e.nativeEvent.layerY,
-                    tView['_viewRect'][2], tView['_viewRect'][3]
-                ],
-                tController
-            )
-            startPosition2 = [tTransformController.x, tTransformController.y, tTransformController.z]
-            if (tController.camera) tController.needUpdate = false
-            document.body.addEventListener(
-                'mousemove', hd_move
-            )
-            window.addEventListener('click', function () {
-                if (tController.camera) tController.needUpdate = true
-                document.body.removeEventListener(
-                    'mousemove', hd_move
-                )
-            })
+                window.addEventListener('click', function () {
+                    if (tController.camera) tController.needUpdate = true
+                    document.body.removeEventListener(
+                        'mousemove', hd_move
+                    )
+                })
+            });
         })
+
     }
     Object.freeze(RedTransformController);
 })();
